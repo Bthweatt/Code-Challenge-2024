b3cf2cf99deaa0fd42bd54d8f5283e167e910754
Brandon Thweatt <bthweatt@students.ChattahoocheeTech.edu>
2024-02-14 21:50:05 -0500

    Final commit

--- src/javascripts/layout-testimonial-slider.js
+++ src/javascripts/layout-testimonial-slider.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const previousBtn = document.querySelector('.previous');
     const nextBtn = document.querySelector('.next');
     let currentSlideIndex = 0;
-	let slideInterval;
-
+    let slideInterval;
+    //Function that checks slide index position
     function showSlide(index) {
         slides.forEach((slide, i) => {
             if (i === index) {
@@ -14,35 +14,35 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
+    // Function called on next slide button
+    function goToPreviousSlide() {
+        const activeSlide = document.querySelector('.slide.active');
+        const previousSlide = activeSlide.previousElementSibling || slides[slides.length - 1];
 
-	function goToPreviousSlide() {
-		const activeSlide = document.querySelector('.slide.active');
-		const previousSlide = activeSlide.previousElementSibling || slides[slides.length - 1];
-	
-		activeSlide.classList.add('transition-out-previous'); 
-	
-		// Listen for the transitionend event on the active slide
-		activeSlide.addEventListener('transitionend', function transitionEndHandler() {
-			activeSlide.classList.remove('transition-out-previous', 'active');
-			previousSlide.classList.add('active'); 
-			activeSlide.removeEventListener('transitionend', transitionEndHandler); 
-		});
-	}
-	
-	function goToNextSlide() {
-		const activeSlide = document.querySelector('.slide.active');
-		const nextSlide = activeSlide.nextElementSibling || slides[0];
-	
-		activeSlide.classList.add('transition-out-next'); 
-	
-		// Listen for the transitionend event on the active slide
-		activeSlide.addEventListener('transitionend', function transitionEndHandler() {
-			activeSlide.classList.remove('transition-out-next', 'active');
-			nextSlide.classList.add('active'); // Set the next slide as active after the animation completes
-			activeSlide.removeEventListener('transitionend', transitionEndHandler); // Remove the event listener to avoid memory leaks
-		});
-	}
-	function startSlideInterval() {
+        activeSlide.classList.add('transition-out-previous');
+
+        // Listen for the transitionend event on the active slide
+        activeSlide.addEventListener('transitionend', function transitionEndHandler() {
+            activeSlide.classList.remove('transition-out-previous', 'active');
+            previousSlide.classList.add('active');
+            activeSlide.removeEventListener('transitionend', transitionEndHandler);
+        });
+    }
+    // Function called on next slide button
+    function goToNextSlide() {
+        const activeSlide = document.querySelector('.slide.active');
+        const nextSlide = activeSlide.nextElementSibling || slides[0];
+
+        activeSlide.classList.add('transition-out-next');
+
+        // Listen for the transitionend event on the active slide
+        activeSlide.addEventListener('transitionend', function transitionEndHandler() {
+            activeSlide.classList.remove('transition-out-next', 'active');
+            nextSlide.classList.add('active'); // Set the next slide as active after the animation completes
+            activeSlide.removeEventListener('transitionend', transitionEndHandler); // Remove the event listener to avoid memory leaks
+        });
+    }
+    function startSlideInterval() {
         slideInterval = setInterval(goToNextSlide, 5000); // Change slide every 5 seconds
     }
 
@@ -65,8 +65,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Start the auto-switching timer when the page loads
     startSlideInterval();
-	
-	
+
+
 
     previousBtn.addEventListener('click', goToPreviousSlide);
     nextBtn.addEventListener('click', goToNextSlide);
